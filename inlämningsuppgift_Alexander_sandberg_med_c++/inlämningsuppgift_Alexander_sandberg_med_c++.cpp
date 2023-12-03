8f57fececa50901096d1a6cfe84511a21877e424
Alexander Sandberg <Alexandersandbergnya@gmail.com>
2023-12-03 16:30:49 +0100

    Add project files.

---
+++ inlämningsuppgift_Alexander_sandberg_med_c++/inlämningsuppgift_Alexander_sandberg_med_c++.cpp
@@ -0,0 +1,13 @@
+ #include <iostream>
+
+int main()
+{
+	while (true)
+	{
+		std::cout << "om du vill stänga av programet skiv" "stop""!\n";
+		break;
+	}
+ 
+}
+
+ 
---
+++ inlämningsuppgift_Alexander_sandberg_med_c++/logik.cpp
@@ -0,0 +1,1 @@
+#include "logik.h"
