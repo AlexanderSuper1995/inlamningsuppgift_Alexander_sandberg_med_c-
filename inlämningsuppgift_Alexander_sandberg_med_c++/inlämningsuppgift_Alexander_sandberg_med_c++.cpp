7c2fac50130bfc88edb067483b24c4a79c459831
Alexander Sandberg <Alexandersandbergnya@gmail.com>
2023-12-07 20:00:19 +0100

    kommer skiva tester snart

--- inlämningsuppgift_Alexander_sandberg_med_c++/inlämningsuppgift_Alexander_sandberg_med_c++.cpp
+++ inlämningsuppgift_Alexander_sandberg_med_c++/inlämningsuppgift_Alexander_sandberg_med_c++.cpp
@@ -5,10 +5,10 @@
 #include <String>
 #pragma execution_character_set( "utf-8" )
 #include "logik.h"
-
 using namespace std;
-int main()
+int main(int argc, char** argv)
 {
+	 
 	  
 	logik logikObj = *new (logik);
     SetConsoleOutputCP(65001);
@@ -21,7 +21,7 @@ int main()
 			std::cout << "du skrev men stängas av måste du skiva stop\n";
 			logikObj.antalTecken_metod(input);
 			logikObj.antal_Rader_metod();
-			if (input=="stop")
+ 			if (input=="stop")
 			{
 				break;
 			}
@@ -30,8 +30,8 @@ int main()
 std::cout<< "antalantalTecken "<< logikObj.antalTecken<<" antal rader "<< logikObj.antal_Rader<<" om du vill stänga förstert skiv stop\n";
 // för att man ska kunna se det som exe annas försvinner förstert så fort att man inte ser något
 std::cin >> input;
-
-return 0;
+//continue;
+ return 0;
 	}
 	 
 	 
